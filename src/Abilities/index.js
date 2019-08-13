3bea3d68c7e4dfd79ffe9eb8a9f22db03393e6ad
Joshua Cox <jnokoi@gmail.com>
2019-08-12 20:25:43 -0700

    fix: added slap to index

--- src/Abilities/index.js
+++ src/Abilities/index.js
@@ -3,3 +3,4 @@ export { default as Bite } from './Bite';
 export { default as Bash } from './Bash';
 export { default as Slash } from './Slash';
 export { default as DoubleSlash } from './DoubleSlash';
+export { default as Slap } from './Slap';
