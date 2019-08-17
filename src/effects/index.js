cbf80e27b12d97a756b7e9fd63f19e25a20fcb72
Joshua Cox <jnokoi@gmail.com>
2019-08-16 21:49:25 -0700

    feature: added poison

--- src/effects/index.js
+++ src/effects/index.js
@@ -1,10 +1,38 @@
-export class Stunned 
+class Effect
 {
   constructor()
   {
+    this.name = 'NA';
+    this.description = 'NA';
+    this.turns = 0;
+    this.count = 0;
+  } 
+}
+
+
+export class Stunned extends Effect
+{
+  constructor(props)
+  {
+    super(props);
+
     this.name = 'Stun';
     this.description = 'Feeling dizzy and stupid.';
     this.turns = 2;
+    this.count = 0;
+  }
+}
+
+
+export class Poisonned extends Effect
+{
+  constructor(props)
+  {
+    super(props);
+    
+    this.name = 'Poison';
+    this.description = 'Feeling like you were bit by Gary Busey.';
+    this.turns = 3;
     this.count = 0;
   }
 }
